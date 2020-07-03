ebbeec9cd9bcacea12ee61b610c9d5851e686f65
David Romanowski <david@braindepotus.com>
2020-07-03 02:33:43 -0400

    updates to test lambda script, find items of correct type that expire within 15 minutes

--- lambda_test1.py
+++ lambda_test1.py
@@ -1,25 +1,31 @@
-#
+# skeleton script for retrieving items that expire within a certain timeframe, default 15 minutes
 import time
+import datetime
 import boto3
 from boto3.dynamodb.conditions import Key
 
-millis = int(round(time.time() * 1000))
+time_in_milli = int(round(time.time() * 1000))
 myTime = time.time()
-print(millis)
+print(time_in_milli)
+minutes_to_monitor = 15
+monitor_time = time_in_milli + minutes_to_monitor * 60 * 1000
+# monitor_time = 1593413148482 ; testing
+print(monitor_time)
 
-
-def query_auctions():
-    if not dynamodb:
-        dynamodb = boto3.resource('dynamodb', endpoint_url="https://dynamodb.us-west-2.amazonaws.com")
+def query_auctions(item_name):
+    # if not dynamodb:
+    dynamodb = boto3.resource('dynamodb', endpoint_url="https://dynamodb.us-west-2.amazonaws.com")
 
     table = dynamodb.Table('Auctions')
     result = table.query(
       KeyConditionExpression=Key('item_name').eq(item_name)
     )
+    return result['Items'] # Need to return the correct Items from the table ...
 
 
 if __name__ == '__main__':
-    item = 'Wise Dragon Helmet'
-    auctions_items = query_auctions(item)
+    item_name = 'Wise Dragon Helmet' # This variable needs to be an option to the user
+    auctions_items = query_auctions(item_name)
     for auction_item in auctions_items:
-        print(auction_item['item_name'], ":", auction_item['end'])
+        if auction_item['end'] <= monitor_time:
+            print(auction_item['item_name'], ":", auction_item['end'], ":", auction_item['starting_bid'], ":", auction_item['highest_bid_amount'])
