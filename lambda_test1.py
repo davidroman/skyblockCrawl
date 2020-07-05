eb784afb099d1044bc68d00b4420d1c7c1dd4a22
David Romanowski <david@braindepotus.com>
2020-07-05 11:41:35 -0400

    reconfigure using lambda_handler function for AWS lambda ... etc

--- lambda_test1.py
+++ lambda_test1.py
@@ -1,7 +1,7 @@
 # skeleton script for retrieving items that expire within a certain timeframe, default 15 minutes
 import time
-import datetime
 import boto3
+import json
 from boto3.dynamodb.conditions import Key
 
 time_in_milli = int(round(time.time() * 1000))
@@ -22,10 +22,29 @@ def query_auctions(item_name):
     )
     return result['Items'] # Need to return the correct Items from the table ...
 
-
-if __name__ == '__main__':
+def lambda_handler(event, context):
+    # Go
+    response = {}
     item_name = 'Wise Dragon Helmet' # This variable needs to be an option to the user
     auctions_items = query_auctions(item_name)
+    counter = 1
+    ## calculate how long until item expires ... expires_at = ; and include in the response
     for auction_item in auctions_items:
         if auction_item['end'] <= monitor_time:
             print(auction_item['item_name'], ":", auction_item['end'], ":", auction_item['starting_bid'], ":", auction_item['highest_bid_amount'])
+            myKey = auction_item['item_name'] +  auction_item['end']
+            myValue = auction_item['highest_bid_amount']
+            response = response.update({myKey : myValue})
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
+
+if __name__ == '__main__':
+    ## item_name = 'Wise Dragon Helmet' # This variable needs to be an option to the user
+    ## auctions_items = query_auctions(item_name)
+    lambda_handler(event, {}) # event ... ?
+    ## for auction_item in auctions_items:
+    ##     if auction_item['end'] <= monitor_time:
+    ##         print(auction_item['item_name'], ":", auction_item['end'], ":", auction_item['starting_bid'], ":", auction_item['highest_bid_amount'])
